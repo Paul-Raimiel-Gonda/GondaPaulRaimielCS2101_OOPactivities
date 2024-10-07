c279bd97f4234cd29bdec91cdc708f63c08b70a5
Paul-Raimiel-Gonda <157487413+Paul-Raimiel-Gonda@users.noreply.github.com>
2024-10-07 10:03:45 +0800

    car car car car fast

--- Exercise Sets/Exercises - Encapsulation/Car.java
+++ Exercise Sets/Exercises - Encapsulation/Car.java
@@ -9,46 +9,21 @@ public class Car {
         this.size = Character.toUpperCase(size);
     }
 
-    public String getColor() {
-        return color;
-    }
-    public void setColor(String color) {
-        this.color = color;
-    }
-    public double getPrice() {
-        return price;
-    }
-    public void setPrice(double price) {
-        this.price = price;
-    }
-    public char getSize() {
-        return size;
-    }
-    public void setSize(char size) {
-        this.size = Character.toUpperCase(size);
-    }
+    public String getColor() { return color; }
+    public void setColor(String color) { this.color = color; }
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
+    public char getSize() { return size; }
+    public void setSize(char size) { this.size = Character.toUpperCase(size); }
 
     @Override
     public String toString() {
-        String sizeDescriptor;
-        switch (size) {
-            case 'S':
-                sizeDescriptor = "small";
-                break;
-            case 'M':
-                sizeDescriptor = "medium";
-                break;
-            case 'L':
-                sizeDescriptor = "large";
-                break;
-            default:
-                sizeDescriptor = "N/A";
-        }
+        String[] sizes = {"N/A", "small", "medium", "large"};
+        String sizeDescriptor = size == 'S' ? sizes[1] : size == 'M' ? sizes[2] : size == 'L' ? sizes[3] : sizes[0];
         return String.format("Car (%s) - P%.2f - %s", color, price, sizeDescriptor);
     }
 
     public static void main(String[] args) {
-        // Test cases
         Car car1 = new Car("purple", 875000.85, 'm');
         System.out.println(car1);
 
