93107956a447a1872b3772818fbfeb19f7b00fb1
Raim Gonda <157487413+Paul-Raimiel-Gonda@users.noreply.github.com>
2024-11-18 16:53:28 +0800

    Fixed Artist.java

--- Laboratory Activities/Laboratory Activity 3 - {18}-{11}-{2024}/Artists/Artist.java
+++ Laboratory Activities/Laboratory Activity 3 - {18}-{11}-{2024}/Artists/Artist.java
@@ -9,4 +9,21 @@ public abstract class Artist {
         this.specialty = specialty;
     }
     public abstract void displayDetails();
+
+    public enum Genre {
+        POP, ROCK, CLASSICAL, JAZZ, HIPHOP, RNB
+    }
+
+    public enum Medium {
+        OIL, WATERCOLOR, ACRYLIC, PENCIL, CHARCOAL
+    }
+
+    public enum WritingStyle {
+        FICTION, NONFICTION, POETRY, DRAMA
+    }
+
+    public enum DanceStyle {
+        BALLET, HIPHOP, JAZZ, CONTEMPORARY
+    }
 }
+
