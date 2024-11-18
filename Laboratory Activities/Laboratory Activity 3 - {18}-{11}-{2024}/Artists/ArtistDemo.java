dd97ce4fa04f88debe4be6959138331dc12b2376
Raim Gonda <157487413+Paul-Raimiel-Gonda@users.noreply.github.com>
2024-11-18 16:53:47 +0800

    Update ArtistDemo.java

--- Laboratory Activities/Laboratory Activity 3 - {18}-{11}-{2024}/Artists/ArtistDemo.java
+++ Laboratory Activities/Laboratory Activity 3 - {18}-{11}-{2024}/Artists/ArtistDemo.java
@@ -1,9 +1,9 @@
 public class ArtistDemo {
     public static void main(String[] args){
-        Singer singer = new Singer("Thom Yorke", 56, "Music", "Art Rock");
-        Dancer dancer = new Dancer("Jabbawockeez", 19, "Dance", "Hip-Hop");
-        Painter painter = new Painter("Gustav Klimt", 55, "Painting", "Art Nouveau");
-        Writer writer = new Writer("Fyodor Dostoevsky", 59, "Literature", "Existential Fiction" );
+        Singer singer = new Singer("Thom Yorke", 56, "Music", Artist.Genre.ROCK);
+        Dancer dancer = new Dancer("Jabbawockeez", 19, "Dance", Artist.DanceStyle.HIPHOP);
+        Painter painter = new Painter("Gustav Klimt", 55, "Painting", Artist.Medium.OIL);
+        Writer writer = new Writer("Fyodor Dostoevsky", 59, "Literature", Artist.WritingStyle.FICTION );
 
         Artist[] artists = {singer, dancer, painter, writer};
 
